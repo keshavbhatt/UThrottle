66aaf2159f32f675da4232df6c794f44d3444137
keshavbhatt <keshavnrj@gmail.com>
2015-10-03 13:26:34 +0530

    about dialog

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -1,3 +1,13 @@
+/*
+*This file is part of UThrottle.
+*
+*UThrottle is not a free software: you can't redistribute it nor modify
+*it without the author <keshavnrj@gmail.com> Keshav Bhatt.
+*
+*UThrottle is distributed in the hope that it will be useful,
+*but WITHOUT ANY WARRANTY; without even the implied warranty of
+*MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
 #include "aboutdialog.h"
 #include "ui_aboutdialog.h"
 
